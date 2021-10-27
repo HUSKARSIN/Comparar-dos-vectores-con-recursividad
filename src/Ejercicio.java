0fc7e4a882cb5c593bf6ea6c05f7217d63db6ac3
HUSKARSIN <mishirigami@gmail.com>
2021-10-27 09:06:41 -0400

    maximo de un vector con recursividad

--- src/Ejercicio.java
+++ src/Ejercicio.java
@@ -34,4 +34,27 @@ public class Ejercicio {
     public static boolean compararVectores(int[] vector1, int[]  vector2){
         return compararVectores(vector1,vector2,0) == 0;
     }
+    /*
+    metodo recursivo para calcular el maximo valor de un vector
+    vector = {2,9,6,7,6,3,4,3}
+     */
+    private static int maximoVector(int[] vector,int indice,int maximo){
+        if (indice == vector.length - 1){//caso base
+            if (vector[indice] > maximo){
+                maximo = vector[indice];
+            }
+        } else{//llamada a la recuesividad
+            if (vector[indice] > maximo){
+                maximo = vector[indice];
+            }
+            maximo = maximoVector(vector,indice + 1,maximo);
+        }
+        return maximo;
+    }
+    /*
+    metodo auxiliar que llama al metodo recursivo
+     */
+    public static int maximoVector(int[] vector){
+       return maximoVector(vector,0,0);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         //System.out.println(Ejercicio.invertirCadena("Recuesividad"));
-        int[]vector1 = {5,6,2};
-        int[]vector2 = {5,6,2};
-        System.out.println(Ejercicio.compararVectores(vector1,vector2));
+
+        // int[]vector1 = {5,6,2};
+        // int[]vector2 = {5,6,2};
+        // System.out.println(Ejercicio.compararVectores(vector1,vector2));
+
+        int [] vector = {2,4,6,7,6,3,4,3};
+        System.out.println(Ejercicio.maximoVector(vector));
     }
 }
